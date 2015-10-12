87a5c19aa20f0c7e0cbd65445a25d9f9abbc43b7
aspafenix <marcusfenix80@mail.ru>
2015-10-12 18:38:20 +0300

    Close #2

---
+++ HomeWork #1/CeasarCipher/Ceasar.Data/CeasarCipher.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Ceasar.Data
+{
+    public class CeasarCipher
+    {
+        private readonly int offset;
+
+        private readonly int MinValue = 32; // defines the 'space' symbol
+        private readonly int MaxValue = 126; // defines the '~' symbol
+
+        public CeasarCipher(int offset)
+        {
+            this.offset = offset;
+        }
+
+        public string Encrypt(string text)
+        {
+            
+            if ((text != null))
+            {
+                char[] symbols = text.ToCharArray();
+
+                for (int i = 0; i < symbols.Length; i++)
+                {
+                    if (symbols[i] >= MinValue && symbols[i] <= MaxValue)
+                    {
+                        if (symbols[i] != 32)
+                        {
+                            if (!((symbols[i] + offset) >= MaxValue))
+                            {
+                                symbols[i] = (char) (symbols[i] + offset);
+                            }
+                            else
+                            {
+                                symbols[i] = (char) (symbols[i] + offset - MaxValue + MinValue);
+                            }
+
+                        }
+                    }
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException(text);
+                    }
+
+                }
+                text = new string(symbols);
+            }
+            else
+            {
+                throw new ArgumentNullException("text");
+            }
+
+            return text;
+        }
+
+        public string Decrypt(string text)
+        {
+            if (text != null)
+            {
+                char[] symbols = text.ToCharArray();
+
+                for (int i = 0; i < symbols.Length; i++)
+                {
+                    if (symbols[i] >= MinValue && symbols[i] <= MaxValue)
+                    {
+                        if (symbols[i] != MinValue)
+                        {
+                            if (!((symbols[i] - offset) <= MinValue))
+                            {
+                                symbols[i] = (char) (symbols[i] - offset);
+                            }
+                            else
+                            {
+                                symbols[i] = (char) (symbols[i] - offset - MinValue + MaxValue);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException(text);
+                    }
+                }
+                text = new string(symbols);
+            }
+
+            else
+            {
+                throw new ArgumentNullException("text");
+            }
+
+            return text;
+        }  
+    }
+}
---
+++ HomeWork #1/CeasarCipher/CeasarCipher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CeasarCipher
+{
+    class Program
+    {
+
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input text");
+            var text = Console.ReadLine();
+            
+            Console.WriteLine("Input offset!");
+            var offset = Console.ReadLine();
+
+            Ceasar.Data.CeasarCipher ceasar = new Ceasar.Data.CeasarCipher(Convert.ToInt32(offset));
+            var encrypted = ceasar.Encrypt(text);
+            Console.WriteLine(encrypted);
+            Console.ReadKey();
+        }
+    }
+}
